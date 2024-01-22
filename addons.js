98d649912013356d1927cba16a383486a58a8d23
Snow <135516526+BabyBoySnow@users.noreply.github.com>
2024-01-22 12:42:20 -0600

    Addons setup and textAreaUnderline

---
+++ addons.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs');
+const sass = require('node-sass');
+const util = require('util');
+const path = require('path');
+
+const addons = './src/addons';
+const outputPath = './public'
+const data = `\
+$version: 'x';
+@import 'lib/selectors/selectorPlaceholders';
+@import 'src/functions';
+@import 'src/variables';
+@import 'src/mixins';
+@import 'src/addons/%s';`;
+
+fs.mkdirSync(outputPath);
+fs.readdirSync(addons).forEach(file => {
+    const name = path.parse(file).name;
+    sass.render({
+        data: util.format(data, name),
+        outputStyle : 'expanded',
+        sourceMap: false,
+    }, function(err, result) {
+        if(err) console.log(err);
+        if(result) {
+            fs.writeFile(`${outputPath}/${name}.css`, result.css, err => {
+                if(err) console.log(err);
+                else console.log(`Successfully wrote ${name}.css`);
+            });
+        }
+    });
+});
